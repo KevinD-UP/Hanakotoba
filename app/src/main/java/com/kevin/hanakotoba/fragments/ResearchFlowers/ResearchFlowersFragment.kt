4ae125391c8e43d68c3bf442260c726a7bb29d71
Alex <alexandrely98@yahoo.com>
2021-11-12 20:25:14 +0100

    New class : UserFlowerDescriptionFragment.kt Added buttons UI Update

--- app/src/main/java/com/kevin/hanakotoba/fragments/ResearchFlowers/ResearchFlowersFragment.kt
+++ app/src/main/java/com/kevin/hanakotoba/fragments/ResearchFlowers/ResearchFlowersFragment.kt
@@ -1,17 +1,18 @@
 package com.kevin.hanakotoba.fragments.ResearchFlowers
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kevin.hanakotoba.R
 import com.kevin.hanakotoba.data.FlowerViewModel
 import com.kevin.hanakotoba.databinding.FragmentResearchFlowersBinding
-import com.kevin.hanakotoba.fragments.MyFlowers.MyFlowersAdapter
+
+
 
 class ResearchFlowersFragment : Fragment() {
     private lateinit var binding : FragmentResearchFlowersBinding
@@ -25,7 +26,7 @@ class ResearchFlowersFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_research_flowers,container,false)
         binding = FragmentResearchFlowersBinding.bind(view)
 
-        val adapter = MyFlowersAdapter()
+        val adapter = ResearchFlowersAdapter()
         binding.rvResearchFlowers .adapter = adapter
         binding.rvResearchFlowers.layoutManager = LinearLayoutManager(requireContext())
 
