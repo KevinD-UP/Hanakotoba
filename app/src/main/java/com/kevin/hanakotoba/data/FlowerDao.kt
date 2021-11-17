29c825d9241c2e2dc47ebd21c5afe01379b26e31
Kévin DANG <kevindang@hotmail.fr>
2021-11-18 00:07:50 +0100

    renaming and deletion

--- app/src/main/java/com/kevin/hanakotoba/data/FlowerDao.kt
+++ app/src/main/java/com/kevin/hanakotoba/data/FlowerDao.kt
@@ -1,6 +1,5 @@
 package com.kevin.hanakotoba.data
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -8,15 +7,15 @@ import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 /**
- * The Data Access Object for the Plant class.
+ * The Data Access Object for the Flower class.
  */
 @Dao
-interface PlantDao {
+interface FlowerDao {
     @Query("SELECT * FROM flower ORDER BY name")
     fun getPlants(): Flow<List<Flower>>
 
-    @Query("SELECT * FROM flower WHERE id = :plantId")
-    fun getPlant(plantId: String): Flow<Flower>
+    @Query("SELECT * FROM flower WHERE flower_id = :flowerId")
+    fun getPlant(flowerId: String): Flow<Flower>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(plants: List<Flower>)
