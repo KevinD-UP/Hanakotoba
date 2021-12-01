8914dcb3c8ca373888fa52fc0ea8f884bc237f4a
Kévin DANG <kdang@pitchy.fr>
2021-12-01 22:30:55 +0100

    Add flower Description view model

---
+++ app/src/main/java/com/kevin/hanakotoba/viewmodels/FlowerDescriptionViewModel.kt
@@ -0,0 +1,13 @@
+package com.kevin.hanakotoba.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kevin.hanakotoba.data.Flower
+import com.kevin.hanakotoba.data.FlowerRepository
+import javax.inject.Inject
+
+class FlowerDescriptionViewModel @Inject internal constructor(
+) : ViewModel() {
+
+}
